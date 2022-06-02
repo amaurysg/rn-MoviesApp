f418072e333477cb5a03e65e4745d7d30aada1bc
Amaury Soto <amaurysotog@gmail.com>
2022-06-02 12:14:11 -0500

    MoviesApp: last details card item

--- src/components/CastItem.tsx
+++ src/components/CastItem.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Image, Text, View} from 'react-native';
+import {Image, StyleSheet, Text, View} from 'react-native';
 import {Cast} from '../interface/creditsInterface';
 
 interface Props {
@@ -7,15 +7,49 @@ interface Props {
 }
 
 export const CastItem = ({actor}: Props) => {
-    const uri = `https://image.tmdb.org/t/p/w500${actor.profile_path}`;
-    console.log(actor.profile_path)
+  const uri = `https://image.tmdb.org/t/p/w500${actor.profile_path}`;
+  console.log(actor.profile_path);
   return (
-  <View>
-   { actor.profile_path &&  <Image source={{uri:uri}} style={{width:50, height:50, borderRadius:50}}/>}
-        <View>
-          <Text>{actor.name}</Text>
-          <Text>{actor.character}</Text>
-        </View>
-  </View>
+    <View style={styles.cardContainer}>
+      {actor.profile_path && (
+     <View style={styles.imageContainer}>
+            <Image
+              source={{uri: uri}}
+              style={styles.image}
+            />
+     </View>
+      )}
+      <View>
+        <Text numberOfLines={1} ellipsizeMode='tail'>{actor.name}</Text>
+        <Text numberOfLines={0} ellipsizeMode='tail'>{actor.character}</Text>
+      </View>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+    cardContainer:{
+       borderWidth:1,  
+       borderColor:'grey',
+       width:80,
+       height:140,
+       alignItems:'center',
+       borderTopEndRadius:12,
+       borderTopStartRadius:12,
+       marginRight:5,
+       marginBottom:30,
+       //justifyContent:'center' 
+    },
+    imageContainer:{
+        width:'100%',
+        height:60,
+        overflow:'hidden',
+    },
+    image:{
+        flex:1,
+        borderTopEndRadius:12,
+        borderTopStartRadius:12,
+      
+    },
+    
+})
